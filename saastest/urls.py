70ab1caacd3c7d5e183bc6f2943c72fe3d49db9d
abu-jafor <abujaforwd@yahoo.com>
2024-11-13 08:52:52 +0600

    prepare for Railway

--- saastest/urls.py
+++ saastest/urls.py
@@ -2,10 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page_view
+from .views import home_view,about_view
 
 urlpatterns = [
-    path('',home_page_view),
-    path('akil/',home_page_view),
+    path('',home_view),
+    path('akil/',about_view),
     path('admin/', admin.site.urls),
 ]
